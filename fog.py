fd7436da67d383d8835611c563f90b8d64cc91c2
strangehood <55440847+strangehood@users.noreply.github.com>
2019-10-07 23:59:28 +0300

    created file fog.py this file is about animation of fog: add f-ns: fog() and fog_animation()

--- fog.py
+++ fog.py
@@ -1,4 +1,6 @@
 from graph import *
+import random as r
+
 c = canvas()
 
 width = 794
@@ -7,8 +9,20 @@ height = 1123
 windowSize(width, height)
 canvasSize(width, height)
 
-def fog():
+
+def fog_generation(cloud_number):  # func generate cloud_number fog clouds
     pass
 
-def fog_animation():
+
+def fog_animation():  # func animate fog
     pass
+
+
+def fog():  # main func
+    fog_generation()
+    fog_animation()
+    return 0
+
+
+fog()
+run()
